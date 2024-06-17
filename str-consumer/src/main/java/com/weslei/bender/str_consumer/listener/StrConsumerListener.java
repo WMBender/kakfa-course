38f7f1a0ed2bbe7de78d9df4082e18a91d13f4e2
weslei.bender <wesleimb@hotmail.com>
2024-06-17 13:17:53 -0300

    add CustomListener annotation to substitute @KafkaListener

--- str-consumer/src/main/java/com/weslei/bender/str_consumer/listener/StrConsumerListener.java
+++ str-consumer/src/main/java/com/weslei/bender/str_consumer/listener/StrConsumerListener.java
@@ -1,24 +1,24 @@
 package com.weslei.bender.str_consumer.listener;
 
+import com.weslei.bender.str_consumer.custom.CustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @CustomListener(groupId = "group-0")
     public void listener(String message){
        log.info("Received message {}", message);
     }
 
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+    @CustomListener(groupId = "group-0")
     public void log(String message){
         log.info("Received message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @CustomListener(groupId = "group-1")
     public void history(String message){
         log.info("Received message {}", message);
     }
